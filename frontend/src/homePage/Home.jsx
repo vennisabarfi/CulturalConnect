4753e6985ebf086ee25204f566c1b9b192092e7c
veyhn <vennisabarfi@gmail.com>
2024-08-22 21:59:35 -0400

    home page set up. will work on backend tomorrow with some go and python yayy

--- frontend/src/homePage/Home.jsx
+++ frontend/src/homePage/Home.jsx
@@ -23,6 +23,7 @@ export default function Home(){
     return(
         <>
     
+    <main className="responsive home-body">
     <div className="tabs">
   <a className="active">
     <i>home</i>
@@ -59,25 +60,134 @@ export default function Home(){
     <input type="text" className="left-round search-input-1" placeholder="  Business, Event..."      value={businessEvent}   onChange={handleBusinessEventChange}></input>
    
     <input type="text" className="right-round  search-input-2" placeholder="   Location..."         value={location}  onChange={handleLocationEventChange}></input>
-    <button className="search-button large">Submit</button>
+    <button className="search-button large">Search</button>
     </form>
 </div>
 
+<p className="hot-events">Hottest Events</p>
 
-  
+<div className="grid-container">
+
+<article className="no-padding">
+  <div className="grid no-space">
+    <div className="s6">
+      <img className="responsive" src="/beer-and-woman.svg"/>
+    </div>
+    <div className="s6">
+      <div className="padding">
+        <h5>Title</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <nav>
+          <button className="border round">See More</button>
+        </nav>
+      </div>
+    </div>
+  </div>
+</article>
+
+    <article className="no-padding">
+  <div className="grid no-space">
+    <div className="s6">
+      <img className="responsive" src="/beer-and-woman.svg"/>
+    </div>
+    <div className="s6">
+      <div className="padding">
+        <h5>Title</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <nav>
+          <button className="border round">See More</button>
+        </nav>
+      </div>
+    </div>
+  </div>
+</article>
+
+<article className="no-padding">
+  <div className="grid no-space">
+    <div className="s6">
+      <img className="responsive" src="/beer-and-woman.svg"/>
+    </div>
+    <div className="s6">
+      <div className="padding">
+        <h5>Title</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <nav>
+          <button className="border round">See More</button>
+        </nav>
+      </div>
+    </div>
+  </div>
+</article>
+
+</div>
+
+<p className="hot-events">Newest Members</p>
+
+<div className="grid-container">
+
+<article className="no-padding">
+  <div className="grid no-space">
+    <div className="s6">
+      <img className="responsive" src="/beer-and-woman.svg"/>
+    </div>
+    <div className="s6">
+      <div className="padding">
+        <h5>Title</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <nav>
+          <button className="border round">See More</button>
+        </nav>
+      </div>
+    </div>
+  </div>
+</article>
+
+    <article className="no-padding">
+  <div className="grid no-space">
+    <div className="s6">
+      <img className="responsive" src="/beer-and-woman.svg"/>
+    </div>
+    <div className="s6">
+      <div className="padding">
+        <h5>Title</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <nav>
+          <button className="border round">See More</button>
+        </nav>
+      </div>
+    </div>
+  </div>
+</article>
+
+<article className="no-padding">
+  <div className="grid no-space">
+    <div className="s6">
+      <img className="responsive" src="/beer-and-woman.svg"/>
+    </div>
+    <div className="s6">
+      <div className="padding">
+        <h5>Title</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <nav>
+          <button className="border round">See More</button>
+        </nav>
+      </div>
+    </div>
+  </div>
+</article>
+
+</div>
+
+</main>
+
+<hr/>
+<footer>
+<p>This is our footer. I haven't added shit yet.</p>
+</footer>
 
 
 
-  
 
- 
-
-
-
-
-
-   
-       
 
 
        </>
