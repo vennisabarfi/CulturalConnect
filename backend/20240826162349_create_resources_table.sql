bc8e57f3a5c7b7b2f495ea58acdd94bdbb1ac88d
veyhn <vennisabarfi@gmail.com>
2024-08-26 12:24:53 -0400

    backend resources setting up

---
+++ backend/20240826162349_create_resources_table.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE resources(
+    
+)
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
