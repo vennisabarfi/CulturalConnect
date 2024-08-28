af5c7bc2b36189dee8d344fdf5ff5c99dec67367
veyhn <vennisabarfi@gmail.com>
2024-08-28 15:38:30 -0400

    Event handler added for viewing and inserting. fixed date and time parsing issue and tested endpoints. off to create home event handler

--- backend/controllers/events.go
+++ backend/controllers/events.go
@@ -18,8 +18,8 @@ type Events struct {
 	OrganizerName string         `json:"organizer_name"`
 	Description   string         `json:"description"`
 	Location      string         `json:"location"`
-	Date          time.Time      `json:"date"`
-	Time          time.Time      `json:"time"`
+	Date          string         `json:"date"`
+	Time          string         `json:"time"`
 	Website       string         `json:"website"`
 	CreatedAt     string         `json: created_at`
 	DeletedAt     string         `json: deleted_at`
@@ -27,14 +27,27 @@ type Events struct {
 
 // var pool *sql.DB
 
+// parse date handler for database format
+func ParseDate(dateStr string) (time.Time, error) {
+	return time.Parse("2006-01-02", dateStr)
+
+}
+
+// parse time handler for database format
+func ParseTime(timeStr string) (time.Time, error) {
+	return time.Parse("15:04", timeStr)
+
+}
+
+// insert an event
 func InsertEvent(c *gin.Context) {
 	var body struct {
 		DisplayImage  sql.NullString `json:"display_image"`
 		OrganizerName string         `json:"organizer_name" binding:"required"`
 		Description   string         `json:"description" binding:"required"`
 		Location      string         `json:"location" binding:"required"`
-		Date          time.Time      `json:"date" binding:"required"`
-		Time          time.Time      `json:"time" binding:"required"`
+		Date          string         `json:"date" binding:"required"`
+		Time          string         `json:"time" binding:"required"`
 		Website       string         `json:"website"`
 	}
 
@@ -58,6 +71,17 @@ func InsertEvent(c *gin.Context) {
 		Website:       body.Website,
 	}
 
+	//parse event date and time in correct format for postgres database
+	parsedEventDate, err := ParseDate(body.Date)
+	if err != nil {
+		log.Fatal("Error parsing event date: ", err)
+	}
+
+	parsedEventTime, err := ParseTime(body.Time)
+	if err != nil {
+		log.Fatal("Error parsing event time: ", err)
+	}
+
 	//open database connection
 	pool, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
@@ -70,7 +94,7 @@ func InsertEvent(c *gin.Context) {
 
 	query := "INSERT INTO events (display_image, organizer_name, description, location, date, time, website) VALUES($1, $2, $3, $4, $5, $6, $7) Returning ID"
 
-	err = pool.QueryRowContext(ctx, query, event.DisplayImage, event.OrganizerName, event.Description, event.Location, event.Date, event.Time, event.Website).Scan(&event.ID) //due to auto increment
+	err = pool.QueryRowContext(ctx, query, event.DisplayImage, event.OrganizerName, event.Description, event.Location, parsedEventDate, parsedEventTime, event.Website).Scan(&event.ID) //due to auto increment
 
 	if err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{
@@ -89,6 +113,51 @@ func InsertEvent(c *gin.Context) {
 	}
 }
 
+// view all events
 func ViewEvent(c *gin.Context) {
+
+	//open database connection
+	pool, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Error opening database connection")
+	}
+
+	defer pool.Close()
+
+	query := "SELECT * FROM events"
+
+	rows, err := pool.Query(query) //uses ctx internally
+	if err != nil {
+		print(err)
+	}
+	defer rows.Close()
+
+	//initialize array of events
+	var events []Events
+
+	// Loop through rows. finds memory address and map onto databases
+	for rows.Next() {
+		var event Events
+		if err := rows.Scan(&event.ID, &event.DisplayImage,
+			&event.OrganizerName, &event.Description,
+			&event.Location, &event.Date, &event.Time, &event.Website,
+			&event.CreatedAt,
+			&event.DeletedAt); err != nil {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{
+				"Error retrieving events": err,
+			})
+			log.Print("Error retrieving events from database", err)
+			return
+		}
+		events = append(events, event) // append results to events array
+	}
+	if err == sql.ErrNoRows {
+		c.IndentedJSON(http.StatusNotFound, gin.H{
+			"message": "No events found",
+		})
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"Events Found": events,
+	})
 
 }
