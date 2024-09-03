102de53390035b34fbbd5710552c2600fd28fe68
veyhn <vennisabarfi@gmail.com>
2024-09-03 15:37:42 -0400

    Checkbox button like functionality added

--- frontend/src/mediaPage/Media.jsx
+++ frontend/src/mediaPage/Media.jsx
@@ -1,11 +1,32 @@
 import NavigationBar from "../components/NavigationBar";
-
+import "./Media.css"
 export default function Media(){
+
+
+    function checkboxColor(){
+        const checkbox = document.querySelector('.checkbox-input');
+        const checkContainer = document.querySelector('.check-container');
+
+        // add error handling
+        checkbox.addEventListener("change", function(){
+            if (checkbox.checked) {
+                checkContainer.style.backgroundColor = "#e24e67";
+            } else {
+                checkContainer.style.backgroundColor = 'pink';
+            }
+        })
+    }
     return(
 
         <>
         <NavigationBar/>
-        <h1>This is Media</h1>
+        <div className="media-checkbox"></div>
+        <div className="check-container" id="change-color" onClick={checkboxColor}>
+
+        <input type="checkbox" className="checkbox-input" name="cb" id="cb"/>
+        <label className="checkbox-label" htmlFor="cb">Podcasts</label>
+
+        </div>
         </>
 
     );
