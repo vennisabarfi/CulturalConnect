888efeb988a1a4c29f4a947106f7a9549ec328d7
veyhn <vennisabarfi@gmail.com>
2024-09-04 19:10:35 -0400

    Updated with images? let's see

--- frontend/src/mediaPage/Media.jsx
+++ frontend/src/mediaPage/Media.jsx
@@ -1,5 +1,7 @@
 import NavigationBar from "../components/NavigationBar";
-import "./Media.css"
+import "./Media.css";
+import chosen_image from "./chosen-image.jpg";
+
 export default function Media(){
 
     // icon movement
@@ -31,6 +33,20 @@ export default function Media(){
         
     }
 
+    function iconMoveShows() {
+        const icon = document.getElementById('arrow-icon-3');
+        const dropdownMenu = document.getElementById('dropdown-items-3');
+
+        if (icon.style.transform === 'rotate(90deg)'){
+                icon.style.transform = 'rotate(0deg)'; //rotate to 0 degrees
+                dropdownMenu.style.display = 'none'; // hide dropdown menu
+        }else{
+            icon.style.transform = 'rotate(90deg)'; //rotate to 0 degrees
+            dropdownMenu.style.display = 'block'; // hide dropdown menu
+        }
+        
+    }
+
    
     return(
 
@@ -41,8 +57,10 @@ export default function Media(){
             <hr/>
         </div>
        
+      
        <div className="media-container">
         <nav>
+             {/* container for podcasts */}
             <div className="media-section">
                 
             <ul className="dropdown">
@@ -55,7 +73,8 @@ export default function Media(){
                 </div>
                   {/* Dropdown */}
                 <ul id="dropdown-items-1" className="dropdown-items">
-                    <li><a>map a</a></li>
+                    <li><a><img className="dropdown-image" src={chosen_image}></img> We're having gay sex</a></li>
+                    <li><a>Chosen Family Podcast</a></li>
                 </ul>
                 
                 
@@ -68,7 +87,7 @@ export default function Media(){
 
        </div>
 
-
+        {/* container for movies */}
        <div className="media-container">
         <nav>
             <div className="media-section">
@@ -88,6 +107,32 @@ export default function Media(){
                 
                 
             </ul>
+            </div>
+
+        </nav>
+
+       </div>
+
+       <div className="media-container">
+        <nav>
+             {/* container for podcasts */}
+            <div className="media-section">
+                
+            <ul className="dropdown">
+                <div className="drop-section">
+                <li>
+                <svg onClick={iconMoveShows} id="arrow-icon-3" className="arrow" xmlns="http://www.w3.org/2000/svg" height="22px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/></svg>
+                
+                    <a> Shows</a>
+                    </li>
+                </div>
+                  {/* Dropdown */}
+                <ul id="dropdown-items-3" className="dropdown-items">
+                    <li><a>map a</a></li>
+                </ul>
+                
+                
+            </ul>
 
             
             </div>
@@ -95,6 +140,7 @@ export default function Media(){
         </nav>
 
        </div>
+
         </>
 
     );
