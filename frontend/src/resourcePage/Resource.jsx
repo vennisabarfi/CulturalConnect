35c933c93d0f15f96e0b6185823e5b5fd55c6f17
veyhn <vennisabarfi@gmail.com>
2024-09-24 20:05:25 -0400

    set up separate branches for dev and main. working on new features on frontend

--- frontend/src/resourcePage/Resource.jsx
+++ frontend/src/resourcePage/Resource.jsx
@@ -142,6 +142,7 @@ function iconMovePodcast(){
   </div>
 
   {/* Mobile/Smaller Screen Responsive Filter */}
+  
   <div className='toggle-filter'> <span>Filter Resources</span> <svg onClick={iconMovePodcast} id="arrow-icon-1" className="filter-arrow" fill="rgb(139, 21, 84);" xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/></svg> </div>
   <div id='mobile-filter-box' className='mobile-filter-box'>
     {/* input search box */}
